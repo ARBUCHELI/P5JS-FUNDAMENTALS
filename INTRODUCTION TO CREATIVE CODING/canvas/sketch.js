1390709b5f584089f019ca2df87e0097c2eeac58
ARBUCHELI <radio_seattle@hotmail.com>
2024-03-01 15:46:24 +0300

    #introduction-to-creative-coding-with-p5js - setup function

--- INTRODUCTION TO CREATIVE CODING/canvas/sketch.js
+++ INTRODUCTION TO CREATIVE CODING/canvas/sketch.js
@@ -1,7 +1,8 @@
 function setup(){
     // Setup code goes here
     // TODO: Create a canvas that is 400px wide and 400px tall
-    createCanvas(400, 400)
+    createCanvas(400, 400);
+    background(Math.floor(Math.random() * 256));
   }
   
   function draw(){
