acc2e93252d5c89e8372a8f5eebec66e37d35d3b
ARBUCHELI <radio_seattle@hotmail.com>
2024-03-01 15:25:21 +0300

    #introduction-to-creative-coding-with-p5js - canvas

---
+++ INTRODUCTION TO CREATIVE CODING/canvas/sketch.js
@@ -0,0 +1,10 @@
+function setup(){
+    // Setup code goes here
+    // TODO: Create a canvas that is 400px wide and 400px tall
+    createCanvas(400, 400)
+  }
+  
+  function draw(){
+    // Drawing code goes here
+    
+  }
