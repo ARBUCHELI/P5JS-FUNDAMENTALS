0ff58464039fddaec413249cd06ac22b8658d2ae
ARBUCHELI <radio_seattle@hotmail.com>
2024-03-01 16:54:05 +0300

    #introduction-to-creative-coding-with-p5js - draw function

--- INTRODUCTION TO CREATIVE CODING/canvas/sketch.js
+++ INTRODUCTION TO CREATIVE CODING/canvas/sketch.js
@@ -1,11 +1,12 @@
-function setup(){
-    // Setup code goes here
-    // TODO: Create a canvas that is 400px wide and 400px tall
-    createCanvas(400, 400);
-    background(Math.floor(Math.random() * 256));
-  }
-  
-  function draw(){
-    // Drawing code goes here
-    
-  }
+function setup() {
+  // Your setup code goes here
+  createCanvas(400, 400);
+  // Run the draw() function once per second (you will learn about the frameRate() function in a later lesson)
+  frameRate(1);
+  // TODO: Move the background() function below to the draw() function
+}
+function draw() {
+  // Your drawing code goes here
+  // TODO: Set background color to a random gray value
+  background(Math.floor(Math.random() *  256));
+}
