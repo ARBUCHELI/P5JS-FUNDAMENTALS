3e44cbb00a57b71a7a2016657b61316a86350846
ARBUCHELI <radio_seattle@hotmail.com>
2024-03-02 22:06:43 +0300

    #drawing-and-coloring-shapes-with-p5js - ordering

---
+++ INTRODUCTION TO CREATIVE CODING/ordering/sketch.js
@@ -0,0 +1,25 @@
+function setup() {
+  createCanvas(400, 400);
+  // TODO: Remove stroke
+  noStroke();
+}
+
+function draw() {
+  background(220);
+  
+  //TODO: Draw Cheese before Holes
+  fill('#FFFF66');
+  rect(50, 50, 300, 300);
+  // Holes
+  fill(220);
+  circle(100,100, 50);
+  circle(225,140, 60);
+  circle(150,275, 50);  
+  circle(300,250, 20);
+  // Endholes
+  
+  // Cheese
+ 
+  // End cheese
+
+}
